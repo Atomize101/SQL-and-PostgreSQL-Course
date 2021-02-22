66bf3790e5931fc10fda6eabb3439dba855312c7
Chris Patrick <atomize101@gmail.com>
2021-02-22 12:55:39 -0500

    HAVING keyword. Section 5 complete

--- commentsDB.sql
+++ commentsDB.sql
@@ -239,4 +239,23 @@ FROM comments
 GROUP BY user_id;
 
 // Find the number of comments for each photo
+SELECT photo_id, COUNT(*)
+FROM comments
+GROUP BY photo_id;
+
+// Find the number of comments for each photo where the photo_id
+// is less than 3 and the photo has more than 2 comments
+SELECT photo_id, COUNT(*)
+FROM comments
+WHERE photo_id < 3
+GROUP BY photo_id
+HAVING COUNT(*) > 2;
+
+// Find the users where the user has comments on the first 50 photos
+// and the user added more than 20 comments on those photos
+SELECT user_id, COUNT(*)
+FROM comments
+WHERE photo_id < 50
+GROUP BY user_id
+HAVING COUNT(*) > 20;
 
